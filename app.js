40f126b8b267f5e089a2c747726148f1e4c96819
Mudassir Shahzad <mudassarshahzad14@gmail.com>
2020-07-23 22:37:27 +0500

    assignment

---
+++ app.js
@@ -0,0 +1,700 @@
+Chapter 1
+alert("Error! Please enter a valid password.")
+ alert("Welcome to JS Land     Happy Codding!")
+alert("Welcome to JS Land...")
+alert("Happy Coding!") <br>
+alert("Prevent this page from creating additional diologe.")
+alert("Hello... i can run JS through my web browser's console.")
+
+Chapter 2
+username = ;
+myname = "ali khan";
+massage = "Hello World"
+alert(massage)
+name ="Ali Khan";
+alert(name)
+age = 18;
+alert(age)
+course = "web development";
+alert(course)
+a = "PIZZA";
+alert(a , a>=4, a--)
+a = "My email address is";
+email = " example@example.com";
+alert(a  + email)
+a = "I am trying to learn from the Book";
+book = " A Smarter way to  learn javaScript";
+alert(a + book)
+ <p>Yah! I can write HTML content through javaScript</p>
+
+Chapter 3
+age = "I am 20 year old";
+alert(age)
+a = 5;
+b = 4;
+alert(a+b)
+a = 5;
+b = 4;
+alert(a-b) 
+a = 5;
+b = 4;
+alert(a*b)
+a = 5;
+b = 4;
+alert(a/b)
+a = 5;
+b = 4;
+alert(a%b)
+
+Task2
+a = 5;
+b = ++a
+c = a + 7
+d = --c
+e = d % 3
+alert(e)
+
+task3
+ticket_cast = "600";
+total_cast_of_5_ticket = ticket_cast * 5;
+alert("Total cast of 5 ticket is " + total_cast_of_5_ticket)
+c = "45"
+f = ((c*9/5) + 32);
+alert(f)
+f = "48"
+c = ((f-32) * 9/5);
+alert(c)
+
+task4
+price_of_item1 = 450
+quantity_of_item1_is = 4
+price_of_item2 = 560
+quantity_of_item2_is = 5
+shiping_charges = 100
+alert("Price of item 1 is " + price_of_item1)
+alert("Quantity of item 2 is " + quantity_of_item1_is)
+alert("Price of item 2 is " + price_of_item2)
+alert("Quantity of item 2 is " + quantity_of_item2_is)
+alert("Shiping Charges is " + shiping_charges)
+total_cast_is = (price_of_item1 * quantity_of_item1_is) +
+(price_of_item2 * quantity_of_item2_is) + shiping_charges
+alert("Total cast is " + total_cast_is)
+
+task5
+student1
+total_marks = 980
+obtain_marks = 780
+percentage = ((obtain_marks * 100) / total_marks)
+alert("Percentage is " + percentage)
+
+task6
+US_Dollors = 10
+Saudi_Riyals = 25
+convert = ((US_Dollors * 104.80) + (Saudi_Riyals * 28))
+alert("Total currency in PRK: " + convert)
+
+task7
+a = 3;
+c = (((a + 5) * 10) / 2)
+alert(c)
+
+task8
+birth_year = 1996
+current_year = 2020
+age = current_year - birth_year
+alert("Your age is: " + age)
+
+task9
+r = 12;
+c = 2 * 3.142 * r
+a = (3.142 * (r**r))
+alert("The circumference is: " + c)
+alert("The area is: " + a)
+
+task10
+favorite_snack = "chocolate chip"
+current_age = 22
+max_age  = 70
+snack_per_day = 4
+total_snack = ((max_age - current_age) * 365.25) * snack_per_day
+alert("Total rest of your life: " + total_snack)
+ 
+task11
+fovorite_snack = prompt(" Enter your favorite snack")
+current_age = prompt(" Enter your current age ")
+max_age = prompt(" Enter your max age")
+snack_per_day = prompt(" Enter your snack per day")
+total_snack = ((max_age - current_age) * 365.25) * snack_per_day
+alert("Total rest of your life: " + total_snack)
+
+
+ Chapter (6-9)
+task1
+a = 10;
+alert("The value of a is: " + a)
+alert("The value of a++ is: " + ++a)
+
+alert("The value of a is: "  +  a)
+alert("The value of a++ is: " + ++a)
+
+alert("The value of a is: " + a)
+alert( "The value of --a is: "  + --a)
+
+alert("The value of a is: " + a)
+alert( "The value of --a is: "  + --a)
+
+Task2
+a = prompt("Enter your name")
+b = prompt("Enter your greet")
+
+task3
+d = prompt("Enter a number") {
+    for(i = 1; i<=10; i++);
+    alert(d * i = "")
+}
+
+task4
+a = english;
+b = Math;
+c = computer;
+total = 100;
+obtain_marks = ""
+
+
+Chapter9-11
+task1
+var city = prompt("Enter a city");
+if (city === "karachi") {
+    alert("Welcome to karachi");
+}
+else{
+    alert("Welcome to other city.....");
+}
+
+task2
+var gender = prompt("Enter your gender");
+if (gender === "male") {
+    alert("Good morning Sir!!!");
+}
+else {
+    alert("Good morning Ma'am!!");
+}
+
+task3
+var signal = prompt("Enter a signal");
+if (signal === "red") {
+    alert("must stop");
+}
+else if(signal === "yellow") {
+    alert("read to move");
+}
+else {
+    alert("move now");
+}
+
+task4
+var fuel = prompt("Enter a fuel");
+if (fuel < "0.25liters") {
+    alert("Please refil the fuel in your car");
+}
+else{
+    alert("Welcome!");
+}
+
+task5
+a = prompt("enter your marks")
+if (a >= 80) {
+    alert( "Your grade is A+" );
+}
+else if (a >= 70) {
+    alert( "Your grade is A" );
+}
+else if (a >= 60) {
+    alert( "Your grade is B" );
+}
+else if (a >= 50) {
+    alert( "Your grade is C" );
+}
+else if (a >= 40) {
+    alert( "Your grade is D" );
+}
+else{
+    alert("Your are  fail")
+}
+
+task6
+a = prompt("enter your marks")
+if (a > 40) {
+   alert( "It is too hot outside." );
+}
+else if (a > 30) {
+   alert( "The Weather today is Normal." );
+}
+else if (a > 20) {
+     alert( "Today’s Weather is cool." );
+}
+else {
+  alert( "OMG! Today’s weather is so Cool." );
+ }
+
+task7
+a = prompt("first_number")
+b = prompt("operator")
+c = prompt("second_number")
+alert(first_number + operation + second_number)
+
+
+
+Chapter12-13
+task 4
+input = prompt("Enter value")
+if (input === "a"){
+    alert("Yes it's Voval");
+}
+else if (input === "e"){
+    alert("Yes it's Voval");
+}
+else if(input === "i"){
+    alert("Yes it's Voval");
+}
+else if(input === "o"){
+    alert("Yes it's Voval");
+}
+else if (input === "u"){
+    alert("Yes it's Voval");
+}
+else{
+    alert("not a voval");
+}
+
+
+task 6
+
+var hour = 13;
+if (hour < 18) {
+    alert("Good day");
+}
+else {
+   alert("Good evening");
+}
+
+
+Chapter14-16
+task1
+i = 0;
+for(i <= 10; i++);
+alert(i)
+
+
+
+
+
+
+
+Chapter21-25
+Task1
+name1 = prompt("Enter first name")
+name2 = prompt("Enter last name")
+Fullname = (name1 +  name2)
+alert( "Name is:     " + Fullname)
+
+
+task2
+phone = prompt("Enter your favourite phone model")
+text = phone.length;
+alert("My favoruite phone is:  " + phone  +"\n"+ "Length of String:   " + text )
+
+task3
+var name = "pakistani"
+ alert("indux of 'n': " + name.induxOf("n"))
+
+task4
+name = "Hello World"
+alert("Last indux of 'l': " + name.index(9))
+
+
+task5
+name = "pakistani"
+alert("Character at indux 3: " + name[3])
+
+
+task6
+name1 = prompt("Enter first name")
+name2 = prompt("Enter last name")
+Fullname = (name1 +  name2)
+alert("My name is: " + Fullname)
+
+
+task7
+city = "hyderabad"
+name = "islam"
+alert("After replacement:  " + name + city.slice(5, 9))
+
+
+task9
+a = "234"
+num = Number(a);
+b = 345
+m = String(b);
+alert("value: " + num +  "\n" +  "type:   "    +   typeof(num)   +  "\n" +
+ "value: "+ num +  "\n" +  "type:   "    +   typeof(m))
+ 
+
+task10
+name = "peanuts"
+name = name.toUpperCase();
+alert(name)
+
+task11
+name = "javascript"
+name = name.titlecase();
+alert(name)
+
+task12
+var value = 35.75;
+v = Math.round(value)
+alert(v)
+
+task13
+a = prompt("Enter your name")
+if (a === " @ . , ! ") {
+     alert("Enter a valid user name")   
+}   
+else{
+     alert("valid name")
+}
+ 
+
+task17
+name = "Pakistan"
+alert("Last character of input: " + name[7])
+
+
+task18
+string =  "The quick brown fox jumps over the lazy dog" 
+count = string.count("the")
+alert("count of string is" + count)
+
+
+
+Chapter26-30
+task1
+a = prompt("Enter positive integer")
+round = Math.round(a)
+floor = Math.floor(a)
+ceil = Math.ceil(a)
+alert("Number is: "+a+ "\n" + "round of;  "+round+"\n" + "floor is:  "+floor+ "\n"+ "ceil of:   "+ceil)
+
+task2
+a = prompt("Enter negetive integer")
+round = Math.round(a)
+floor = Math.floor(a)
+ceil = Math.ceil(a)
+alert("Number is: "+a+ "\n" + "round of;  "+round+"\n" + "floor is:  "+floor+ "\n"+ "ceil of:   "+ceil)
+
+task3
+a = -4;
+value = Math.abs(a)
+alert("Absolute value of -4 is:   "  + value)
+
+task4
+a = prompt("Enter value")
+num = Math.random(a)
+alert("Random is:  " + num)
+
+
+task5
+d = prompt("Enter value")
+num = Math.coin
+
+
+task6
+for( i = 0;  i<=100;  i++){
+    alert(Math.random(i))
+}
+
+
+task7
+a = prompt("Enter Weight")
+alert("Your weight is:  " + a)
+  
+ 
+task8
+i = prompt("Enter value")
+for(i = 0; i<=10;){
+    alert(i)
+}
+
+
+Chapter31-34
+task1
+d = new Date();
+b = d.getTime();
+c = d.getDate();
+alert("Time is:  "+b +"\n"+"Current date:  "+ c)
+
+task2
+d = new Date();
+b = d.getMonth();
+alert("Current month: " + b)
+
+task3
+d = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+d = new Date();
+b = d.getDay();
+alert("Current day: " + d)
+
+
+task4
+if(day.CurrentDay("saturday || sunday")){
+    alert("it's fun day")
+}
+else{
+    alert("not fun day")
+}
+
+
+task5
+a = prompt("Enter date")
+if(a<16){
+    alert("First fifteen day of the month")
+}
+else{
+    alert("Last day of the month")
+}
+
+task6
+a = prompt("Enter date")
+day = a.getDate();
+mili = a.getmiliSecond();
+min = a.getMinutes();
+alert(a + "day" +day +"mili" + mili + "min" + min)
+
+
+task7
+a = prompt("")
+if(a< 12){
+    alert("it's AM")
+}
+else{
+    alert("it's PM")
+}
+
+task9
+ramzan = "12-3-2020";
+today = "6-4-2020";
+total = (ramzan-today)
+alert(total)
+
+
+Chapter35-38
+task1
+function tellTime() {
+     var now = new Date() ;
+     var theHr = now. getHours() ;
+     var theMin = now. getMinutes() ;
+     var theDate = now.getDate();
+     alert("Current time:   "+ theHr + ": " + theMin + "\n" + "Current Date is:   " +  theDate) ;
+}
+tellTime();
+
+
+task2
+function name(){
+    var fname = prompt("Enter First name ");
+    var lname = prompt("Enter last name");
+    alert("Full Name: " + fname +" " +lname)
+}
+name();
+
+task3
+function add(){
+    var a = prompt("");
+    var b = prompt("");
+    alert( "Sum is: " +  (a+b))
+}
+add();
+
+
+task4
+function calc(num){
+    var a = prompt("Enter first num");
+    var b = prompt("Enter operation");
+    var c = prompt("Enter Second num")
+    var d = a+b+c;
+    alert(d)
+}
+calc();
+
+task5
+function value(){
+    a = prompt("Enter a num");
+    b = a.squr();
+    alert(b)
+}
+value();
+
+
+task10 
+palindrome = prompt("Enter a word")
+function palindrome(str){
+    return;true
+}
+palindrome();
+
+task11
+function word(){
+   a = word.title();
+}
+word();
+
+
+task12
+function myfun(){
+    var str = prompt("Enter a value")
+    console.log(str.length);
+}
+myfun();
+
+
+task13
+function fun(){
+    var str = prompt("Enter a value")
+    a = str
+     alert(str.length;)
+}
+ fun();
+
+
+
+task14
+function calcr(){
+    r = prompt("Enter a radius")
+    c = 2*3.14*r;
+    a = 3.14*r**r
+    alert("Circumference is:  " + c +   "\n" +"Area of Circle is:    "  + a)
+}
+calcr();
+
+Chapter 38-42
+task1
+
+
+
+
+task2
+function year(){
+    if (year === 2012 || year === 2016 || year === 2020){
+        alert("Its leap year")
+    }
+    else{
+        alert("not")
+    }
+}
+year(2000);
+
+
+
+chapter 43-48
+task1
+on Html
+
+task2
+on Html
+
+
+
+
+task3
+var del = document.getElementById("delete")
+
+del = value.delete
+
+
+
+task4
+function swapPic() {
+    document. getElementById("before") . src = "1.JPG";
+    }
+
+
+
+
+task5
+start = 0;
+var startHeading = document.getElementById("start");
+var interval;
+interval = setInterval(plus,1)
+
+function plus(){
+    start++
+    startHeading.innerHTML = start;
+}
+function plus(){
+    start--
+    startHeading.innerHTML = start;
+
+}
+function plus(){
+    interval = setInterval(plus, 1)
+}
+function minus(){
+    interval = setInterval(plus,-1)
+}
+
+
+chapter49-52
+task2
+
+function expandLoris() {
+    var expandedParagraph = "Slow lorises are a group of several species of trepsirrhine primates which
+    make up the genus Nycticebus. They have a round head, narrow snout, large eyes, and a variety of
+    distinctive coloration patterns that are species-dependent. The hands and feet of slow lorises have several
+    adaptations that give them a pincer-like grip and enable them to grasp branches for long periods of time.
+    Slow lorises have a toxic bite, a rare trait among mammals. ";
+    document. getElementById("slowLoris") . innerHTML = expandedParagraph;
+}
+
+
+chapter53-58
+task1
+
+
+function zoomin(){
+    var myImg = document.getElementById("map");
+    var currWidth = myImg.clientWidth;
+    if(currWidth == 2500) return false;
+     else{
+        myImg.style.width = (currWidth + 100) + "px";
+    } 
+}
+function zoomout(){
+    var myImg = document.getElementById("map");
+    var currWidth = myImg.clientWidth;
+    if(currWidth == 100) return false;
+     else{
+        myImg.style.width = (currWidth - 100) + "px";
+    }
+}
+
+
+
+
+
+
+chapter58-67
+task1
+
+var con = document.getElementById("form-content")
+var col = document.getElementsByClassName("render").innerHTML;
+
+document.getElementById("first").value = "Ali";
+document.getElementById("last").value = "Khan";
+document.getElementById("email").value = "ali@khan.com";
+
+var type = node.nodeType;
+
+
+
+console.log(document.childNodes[1].childNodes[2].nodeType)
+console.log(document.childNodes[1].childNodes[2].childNodes[27])
